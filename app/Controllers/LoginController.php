a73b753056881c8aa945e0d25e62e66eeff5884f
qeebza <naqibzull@gmail.com>
2026-07-04 17:06:42 +0800

    Implement login form logic inside controller class

--- app/Controllers/LoginController.php
+++ app/Controllers/LoginController.php
@@ -4,12 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Src\Request;
+
 class LoginController {
+    public function __construct(private Request $request) {
+    }
+
     public function index(): void {
         view("login");
     }
 
     public function login(): void {
-        echo "Login form submitted";
+        $email = $this->request->input("email");
+        $password = $this->request->input("password");
+
+        echo "Email: " . $email;
+        echo "<br>";
+        echo "Password: " . $password;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,6 @@ declare(strict_types=1);
 use App\Controllers\HomeController;
 use App\Controllers\LoginController;
 
-$router->get("/", [new HomeController, "index"]);
-$router->get("/login", [new LoginController, "index"]);
-
-$router->post("/login", function() use($request){
-    $email = $request->input("email");
-    $password = $request->input("password");
-
-    echo "Email: " . $email;
-    echo "<br>";
-    echo "Password: " . $password;
-});
+$router->get("/", [new HomeController(), "index"]);
+$router->get("/login", [new LoginController($request), "index"]);
+$router->post("/login", [new LoginController($request), "login"]);
